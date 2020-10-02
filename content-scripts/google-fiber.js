8fd84382694f53c45355bbfdbcaaa62fc2261a3f
Joe Longstreet <joelongstreet@gmail.com>
2020-10-02 09:54:25 -0500

    wip

---
+++ content-scripts/google-fiber.js
@@ -0,0 +1,3 @@
+const $streetAddress = document.getElementsByName('street_address')[1];
+const $zipCode = document.getElementsByName('zip_code')[1];
+const $button = document.getElementsByClassName('address-checker__submit address-checker__submit--inline glue-button glue-button--high-emphasis')[1];
